3a8a9be5ffd06411cf2046cddfa88712dc1ac1ab
mgmaroa <matikogmaroa@gmail.com>
2026-03-21 10:07:35 +0300

    admin panel customization and register the User site

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+    list_display = ['username', 'email', 'role', 'is_staff']
+
+admin.site.register(User, CustomUserAdmin)
