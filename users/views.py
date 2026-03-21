2638d01e6776c6043cb7dd1df29601779f0c9d34
mgmaroa <matikogmaroa@gmail.com>
2026-03-21 12:21:59 +0300

    create the different user views

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from .permissions import role_required
 
 # Create your views here.
 @login_required
@@ -14,3 +16,23 @@ def dashboard_redirect(request):
         return redirect('registrar_dashboard')
     else:
         return redirect('student_dashboard')
+
+@role_required(['ICT'])
+@login_required
+def ict_dashboard(request):
+    return HttpResponse("ICT Dashboard")
+
+@role_required(['ACCOUNTS'])
+@login_required
+def accounts_dashboard(request):
+    return HttpResponse("Accounts Dashboard")
+
+@role_required(['REGISTRAR'])
+@login_required
+def registrar_dashboard(request):
+    return HttpResponse("Registrar Dashboard")
+
+@role_required(['STUDENT'])
+@login_required
+def student_dashboard(request):
+    return HttpResponse("Student Dashboard")
