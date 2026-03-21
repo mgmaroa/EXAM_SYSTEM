a7e0b1e0d8e8d71b231b8a3881676c179e3eb642
mgmaroa <matikogmaroa@gmail.com>
2026-03-21 11:33:23 +0300

    creates a redirect view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+def dashboard_redirect(request):
+    user = request.user
+
+    if user.role == 'ICT':
+        return redirect('ict_dashboard')
+    elif user.role == 'ACCOUNTS':
+        return redirect('accounts_dashboard')
+    elif user.role == 'REGISTRAR':
+        return redirect('registrar_dashboard')
+    else:
+        return redirect('student_dashboard')
