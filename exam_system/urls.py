223100139242f78aa1ad6503833bcd52ab8fc1c9
mgmaroa <matikogmaroa@gmail.com>
2026-03-21 12:24:08 +0300

    register app urls in urls for the project

--- exam_system/urls.py
+++ exam_system/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')), # Provides login, logout, password reset
+    path('', include('users.urls')),
+    
 ]
