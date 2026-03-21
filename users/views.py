00711ed9cefd4afa66715096af4f2bee67d05e0b
mgmaroa <matikogmaroa@gmail.com>
2026-03-21 18:03:28 +0300

    search student by student id

--- users/views.py
+++ users/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.contrib import messages
+from django.utils import timezone
 from .permissions import role_required
+from .models import StudentProfile
+from .forms import StudentSearchForm
+
+# Assuming 0.00 is the requirement, or adjust to your "Minimum Requirement"
+MINIMUM_BALANCE_THRESHOLD = 10000
 
 # Create your views here.
 @login_required
@@ -25,7 +32,25 @@ def ict_dashboard(request):
 @role_required(['ACCOUNTS'])
 @login_required
 def accounts_dashboard(request):
-    return HttpResponse("Accounts Dashboard")
+    search_form = StudentSearchForm() # Handles the get request i.e display form
+    student_profile = None
+
+    if request.method == 'POST':
+        search_form = StudentSearchForm(request.POST)
+
+        if search_form.is_valid():
+            student_id = search_form.cleaned_data['student_id']
+
+            try:
+                student_profile = StudentProfile.objects.get(student_id=student_id)
+            except StudentProfile.DoesNotExist:
+                messages.error(request, "Student ID not found.")
+    
+    return render(request, 'accounts/dashboard.html', {
+        'search_form': search_form,
+        'student_profile': student_profile
+    })
+
 
 @role_required(['REGISTRAR'])
 @login_required
