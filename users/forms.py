27f2098d27cb51c1284104c804d62cf8763ff634
mgmaroa <matikogmaroa@gmail.com>
2026-07-12 16:31:28 +0300

    adds the student import form

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.core.validators import FileExtensionValidator
 from .models import StudentProfile
+
+MAX_UPLOAD_SIZE = 5 * 1024 * 1024  # 5MB
 
 # Form for searching student details
 class StudentSearchForm(forms.Form):
@@ -13,7 +16,6 @@ class StudentSearchForm(forms.Form):
         )
     )
 
-
 # Form for updating Student data
 class FeeUpdateForm(forms.ModelForm):
     class Meta:
@@ -25,3 +27,17 @@ class FeeUpdateForm(forms.ModelForm):
                 'class': 'form-control'
             })
         }
+
+# Form for uploading Excel file of students
+class StudentImportForm(forms.Form):
+    excel_file = forms.FileField(
+        label="Excel file",
+        validators=[FileExtensionValidator(allowed_extensions=['xlsx', 'xls'])],
+        help_text="Required columns: student_id, full_name, course. Max size 5MB.",
+    )
+
+    def clean_excel_file(self):
+        f = self.cleaned_data['excel_file']
+        if f.size > MAX_UPLOAD_SIZE:
+            raise forms.ValidationError("File is too large. Maximum size is 5MB.")
+        return f
