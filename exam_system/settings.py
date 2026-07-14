8c0dc7be7fe59a36066a6fa52e6706fb45114c04
mgmaroa <matikogmaroa@gmail.com>
2026-07-14 21:41:24 +0300

    incorporates secret key environemnt variable for production:

--- exam_system/settings.py
+++ exam_system/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_w5*k$l_(4(-$d9r+n7*(lazq3^%ifjua=doxl0-955(#ef3$&'
+# SECRET_KEY = 'django-insecure-_w5*k$l_(4(-$d9r+n7*(lazq3^%ifjua=doxl0-955(#ef3$&'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-_w5*k$l_(4(-$d9r+n7*(lazq3^%ifjua=doxl0-955(#ef3$&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
