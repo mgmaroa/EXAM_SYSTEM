5d4c2fffdbbf17198c8b20f712b5d5f1c94e0601
mgmaroa <matikogmaroa@gmail.com>
2026-07-16 17:33:46 +0300

    added localhost to allowed hosts and changed the debug to true for development purposes

--- exam_system/settings.py
+++ exam_system/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-_w5*k$l_(4(-$d9r+n7*(lazq3^%ifjua=doxl0-955(#ef3$&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['mgmaroa.pythonanywhere.com']
+ALLOWED_HOSTS = ['mgmaroa.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
