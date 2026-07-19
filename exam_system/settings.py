0c8bfae3082645f249d71cd52ff14f39df47016c
mgmaroa <matikogmaroa@gmail.com>
2026-07-19 11:55:59 +0300

    saves allowed hosts in an environment variable

--- exam_system/settings.py
+++ exam_system/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['mgmaroa.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
