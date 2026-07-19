fcee08811d6602aea81c32fd789a01f9c9ab2959
mgmaroa <matikogmaroa@gmail.com>
2026-07-19 09:47:44 +0300

    adds session timeout middleware to middleware in settings

--- exam_system/settings.py
+++ exam_system/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'users.middleware.IdleTimeoutMiddleware',  # custom middleware for session expiry
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
